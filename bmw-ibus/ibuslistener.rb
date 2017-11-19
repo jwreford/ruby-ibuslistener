ef564b2125ded2827a48366cfcf33b265137f7e6
Josh Wreford <Josh@A-Tall-Ship.local>
2017-11-19 22:50:20 +1100

    Testing

--- bmw-ibus/ibuslistener.rb
+++ bmw-ibus/ibuslistener.rb
@@ -16,26 +16,29 @@ class IBusListener
       # If we are transmitting a message, don't flash the LEDs because it will go around forever in a loop.
       puts "This Message: #{@message}"
       puts "Last Message: #{@lastMessage}"
-      if @lastMessage.include?("tx") and @lastMessage[3..-1] == @message[3..-1]
-        puts "We sent that message - skipping"
-      else
-        # Flash the Board Monitor LEDs when a message comes in.
-        @ibusListener.puts("tx C804E72B3200")
-        @ibusListener.puts("tx C804E72B0000")
-        # strip the first three characters (the "tx " or "rx ")
-        @message.slice!(0,3)
-        # Make the string uppercase
-        @message.upcase!
-        # Split the string into groups of two characters in an array.
-        @message = @message.scan(/.{1,2}/)
-        @message = IBusMessage.new(@message) # Shove them into a new ibus message object
-        #@message.printRawMessage
-        @message.printDecodedMessage
+      if @lastMessage != ""
+        if @lastMessage.include?("tx") and @lastMessage[3..-1] == @message[3..-1]
+          puts "We sent that message - skipping"
+        else
+          # Flash the Board Monitor LEDs when a message comes in.
+          @ibusListener.puts("tx C804E72B3200")
+          @ibusListener.puts("tx C804E72B0000")
+          # strip the first three characters (the "tx " or "rx ")
+          @message.slice!(0,3)
+          # Make the string uppercase
+          @message.upcase!
+          # Split the string into groups of two characters in an array.
+          @message = @message.scan(/.{1,2}/)
+          @message = IBusMessage.new(@message) # Shove them into a new ibus message object
+          #@message.printRawMessage
+          @message.printDecodedMessage
 
-        # Copy this messag into the @lastMessage variable so we can compare it next time around.
-        @lastMessage = @message
+          # Copy this messag into the @lastMessage variable so we can compare it next time around.
+          ###### Find a way to do this (use )
+          @lastMessage = @message
 
-        @message = nil # destroy the message, ready for the next one.
+          @message = nil # destroy the message, ready for the next one.
+        end
       end
     end
     ibusListener.close             # close socket when done
