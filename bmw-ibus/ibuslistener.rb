938e18c65c592b7e85c61c3bc59a2e16996dd9e9
Josh Wreford <Josh@A-Tall-Ship.local>
2017-11-19 22:42:55 +1100

    Testing

--- bmw-ibus/ibuslistener.rb
+++ bmw-ibus/ibuslistener.rb
@@ -8,15 +8,15 @@ class IBusListener
     #puts "In Initialise for IBusListener"
     # Instance variables
     @ibusListener = TCPSocket.new '127.0.0.1', 55537
-    @@lastMessage = ""
+    @lastMessage = ""
   end
 
   def listen
     while @message = @ibusListener.gets # Read lines from socket
       # If we are transmitting a message, don't flash the LEDs because it will go around forever in a loop.
       puts "This Message: #{@message}"
-      puts "Last Message: #{@@lastMessage}"
-      if @@lastMessage.contain?("tx") and @@lastMessage[3..-1] == @message[3..-1]
+      puts "Last Message: #{@lastMessage}"
+      if @lastMessage.include?("tx") and @lastMessage[3..-1] == @message[3..-1]
         puts "We sent that message - skipping"
       else
         # Flash the Board Monitor LEDs when a message comes in.
