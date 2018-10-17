12aec692f8d375f8acc68889f06c806b8eac756d
Josh Wreford <josh@A-Tall-Ship.local>
2018-10-17 21:58:12 +1100

    Moved IKE into separate class

--- bmw-ibus/ibus.rb
+++ bmw-ibus/ibus.rb
@@ -440,8 +440,8 @@ class IBusMessage
           @methodMessage = IKE.new # Create new IKE object
           @methodMessage.setDecode(@sourceName,@data,@length) # Set variables in IKE object ready for Decoding a message
           @methodMessage.decodeMessage
+          puts "Decoded Message: #{@methodMessage.decodeMessage}"
           puts "Source Source: #{@sourceName}"
-          puts "#{@methodMessage.decodeFunction}"
     elsif DeviceFunctionsIN.key?(@destinationName) == true && @destinationName != "GT"
       # Iterate through the message, starting with on byte. If we don't find a valid method, add the next byte to the end and try again
       @processedData.each { |currentByte|
