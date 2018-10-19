f60e39eeef801e907d4af66bf6a134c380e50bfe
Josh Wreford <josh@A-Tall-Ship.local>
2018-10-20 10:19:21 +1100

    Moved many other into separate class

--- bmw-ibus/glo.rb
+++ bmw-ibus/glo.rb
@@ -94,19 +94,19 @@ class GLO
     @messageData.each { |currentByte|
       bytesCheck.push(currentByte)
       byteCounter = byteCounter + 1
-      puts "[-] -> Checking if #{GLOStaticMessagesIN.key?(bytesCheck)} (Static Message) or #{GLOStaticMessagesIN.fetch(@messageData)} (Function) is True"
+      puts "[-] -> Checking if #{GLOStaticMessagesIN.key?(bytesCheck)} (Static Message) or #{GLOStaticMessagesIN.key?(@messageData)} (Function) is True"
       if GLOStaticMessagesIN.key?(bytesCheck) == true
-        puts "[-] --> Static Message was True. Returning #{GLOStaticMessagesIN.fetch(@messageData)}"
+        puts "[1] --> Static Message was True. Returning #{GLOStaticMessagesIN.fetch(@messageData)}"
         return "#{GLOStaticMessagesIN.fetch(@messageData)}"
       elsif GLOFunctionsIN.key?(bytesCheck) == true
-        puts "[-] --> Function was True. Returning #{GLOStaticMessagesIN.fetch(@messageData)}"
+        puts "[2] --> Function was True. Returning #{GLOStaticMessagesIN.fetch(@messageData)}"
         for i in 1..byteCounter do
           @messageData.shift # Push the 'function' bits off the front of the array, leaving the message content.
         end
-        puts "--> Array:  #{GLOFunctionsIN.fetch(bytesCheck)}. Length: #{GLOFunctionsIN.fetch(bytesCheck).length}"
-        puts "--> Words: #{GLOFunctionsIN.fetch(bytesCheck)[0]}"
-        puts "--> Function: #{GLOFunctionsIN.fetch(bytesCheck)[1]}"
-        puts "--> Bytes Check: #{bytesCheck}. Message Data: #{@messageData}"
+        puts "[2] --> Array:  #{GLOFunctionsIN.fetch(bytesCheck)}. Length: #{GLOFunctionsIN.fetch(bytesCheck).length}"
+        puts "[2] --> Words: #{GLOFunctionsIN.fetch(bytesCheck)[0]}"
+        puts "[2] --> Function: #{GLOFunctionsIN.fetch(bytesCheck)[1]}"
+        puts "[2] --> Bytes Check: #{bytesCheck}. Message Data: #{@messageData}"
         return "#{@messageData}"
       end
       puts "Outside If"
