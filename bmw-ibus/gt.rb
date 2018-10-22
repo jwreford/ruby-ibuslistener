974dccaa5b340d5671b4ab850f2f05f3e1c66894
Josh Wreford <josh@A-Tall-Ship.local>
2018-10-22 21:15:19 +1100

    Fixed GPS Coordinates decoder Added support for Audio Status messages (Bass, Treble, Fader and Balance).

--- bmw-ibus/gt.rb
+++ bmw-ibus/gt.rb
@@ -39,6 +39,8 @@ class GT
     ["A5", "62"] => ["Write To Heading", "readHeading"],
     ["21", "61", "00"] => ["Partial Write To Lower Field", "readLower"],
     ["A5", "60", "01", "00"] => ["Clear Lower Fields", "clearLower"],
+    ["36"] => ["Audio Controls", "decodeAudioControls"]
+
   }
 
   HeadingFields = {
@@ -69,6 +71,116 @@ class GT
     ["FF"] => "Transmission Complete"
   }
 
+  # Sent from the RAD
+  AudioStates = {
+    ## Balance
+    ["4F"] => "Left < 15",
+    ["4E"] => "Left < 14",
+    ["4D"] => "Left < 13",
+    ["4C"] => "Left < 12",
+    ["4B"] => "Left < 11", # Don't think this is used
+    ["4A"] => "Left < 10",
+    # ["XX"] => "Left < 9", ## Don't think there is a Left Balance of 9
+    ["48"] => "Left < 8",
+    ["47"] => "Left < 7", # Don't think this is used
+    ["46"] => "Left < 6", # Don't think this is used
+    ["45"] => "Left < 5",
+    ["44"] => "Left < 4",
+    ["43"] => "Left < 3",
+    ["42"] => "Left < 2",
+    ["41"] => "Left < 1",
+    ["40"] => "Centre = 0",
+    ["51"] => "Right > 1",
+    ["52"] => "Right > 2",
+    ["53"] => "Right > 3",
+    ["54"] => "Right > 4",
+    ["55"] => "Right > 5",
+    ["56"] => "Right > 6", # Don't think this is used
+    ["57"] => "Right > 7", # Don't think this is used
+    ["58"] => "Right > 8",
+    # ["XX"] => "Right > 9", ## Don't think there is a Right Balance of 9 either.
+    ["5A"] => "Right > 10",
+    ["5B"] => "Right > 11", # Don't think this is used
+    ["5C"] => "Right > 12",
+    ["5D"] => "Right > 13",
+    ["5E"] => "Right > 14",
+    ["5F"] => "Right > 15",
+
+    ## Fader
+    ["8F"] => "Front > 15",
+    ["8E"] => "Front > 14",
+    ["8D"] => "Front > 13",
+    ["8C"] => "Front > 12",
+    ["8B"] => "Front > 11", # Don't think this is used
+    ["8A"] => "Front > 10",
+    # ["XX"] => "Left < 9", ## Don't think there is a Left Balance of 9
+    ["88"] => "Front > 8",
+    ["87"] => "Front > 7", # Don't think this is used
+    ["86"] => "Front > 6", # Don't think this is used
+    ["85"] => "Front > 5",
+    ["84"] => "Front > 4",
+    ["83"] => "Front > 3",
+    ["82"] => "Front > 2",
+    ["81"] => "Front > 1",
+    ["90"] => "Centre = 0",
+    ["91"] => "Rear < 1",
+    ["92"] => "Rear < 2",
+    ["93"] => "Rear < 3",
+    ["94"] => "Rear < 4",
+    ["95"] => "Rear < 5",
+    ["96"] => "Rear < 6", # Don't think this is used
+    ["97"] => "Rear < 7", # Don't think this is used
+    ["98"] => "Rear < 8",
+    # ["XX"] => "Right > 9", ## Don't think there is a Right Balance of 9 either.
+    ["9A"] => "Rear < 10",
+    ["9B"] => "Rear < 11", # Don't think this is used
+    ["9C"] => "Rear < 12",
+    ["9D"] => "Rear < 13",
+    ["9E"] => "Rear < 14",
+    ["9F"] => "Rear < 15",
+
+    ## Treble Adjustment
+    ["DC"] => "Treble: -12",
+    ["DA"] => "Treble: -10",
+    ["D8"] => "Treble: -8",
+    ["D6"] => "Treble: -6",
+    ["D4"] => "Treble: -4",
+    ["D2"] => "Treble: -2",
+    ["D0"] => "Treble: Flat (0)",
+    ["C2"] => "Treble: +2",
+    ["C4"] => "Treble: +4",
+    ["C6"] => "Treble: +6",
+    ["C8"] => "Treble: +8",
+    ["CA"] => "Treble: +10",
+    ["CC"] => "Treble: +12",
+
+    ## Bass Adjustment
+    ["DC"] => "Bass: -12",
+    ["7A"] => "Bass: -10",
+    ["78"] => "Bass: -8",
+    ["76"] => "Bass: -6",
+    ["74"] => "Bass: -4",
+    ["72"] => "Bass: -2",
+    ["70"] => "Bass: Flat (0)",
+    ["62"] => "Bass: +2",
+    ["64"] => "Bass: +4",
+    ["66"] => "Bass: +6",
+    ["68"] => "Bass: +8",
+    ["6A"] => "Bass: +10",
+    ["6C"] => "Bass: +12"
+
+  }
+
+  def decodeAudioControls(hex)
+    audioStateResponse = ""
+    if AudioStates.key?(hex) == true
+      audioStateResponse = "Audio Settings Changed: #{Layouts.fetch(hex)}"
+    else
+      audioStateResponse = "Audio State Changed: Unknown (#{hex})"
+    end
+    return audioStateResponse
+  end
+
 
   def readTitle(data)
     currentBit = ""
@@ -88,7 +200,7 @@ class GT
     end
     # Decode Hex
     messageASCII = toAscii2(data)
-    return "Writing to #{messageField}. Content: #{messageASCII}. Layout: #{messageLayout}, Flags: #{messageFlags}"
+    return "Writing to #{messageField} \"#{messageASCII}\". Layout: #{messageLayout}, Flags: #{messageFlags}"
   end
 
   def readHeading(data)
