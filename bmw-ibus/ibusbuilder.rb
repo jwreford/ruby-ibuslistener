6329bcc070300eabcdcd007425c7d29b2b1a0e92
Josh Wreford <josh@A-Tall-Ship.local>
2018-10-25 22:09:08 +1100

    .

--- bmw-ibus/ibusbuilder.rb
+++ bmw-ibus/ibusbuilder.rb
@@ -41,9 +41,9 @@ class IBusBuilder
 
   def buildMessage
     puts "Mssage Details 2: #{@messageDetails}"
-    messageFunctionName = @messageDetails.fetch(functionName)
-    messageFunctionDetails = @messageDetails.fetch(functionDetails) # This is probably going to need to be an array with the appropriate parameters inside
-    messageContent = @messageDetails.fetch(content)
+    messageFunctionName = @messageDetails.fetch("functionName")
+    messageFunctionDetails = @messageDetails.fetch("functionDetails") # This is probably going to need to be an array with the appropriate parameters inside
+    messageContent = @messageDetails.fetch("content")
     puts "====== Building Message ======"
     puts "Source Device: #{@sourceDeviceName} (#{sourceDeviceHex})"
     puts "Dest.. Device: #{@destinationDeviceName} (#{destinationDeviceHex})"
