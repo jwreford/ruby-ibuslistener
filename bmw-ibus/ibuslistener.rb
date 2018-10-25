823a5dd6eeba910709c824a214ee786a745c9d75
Josh Wreford <josh@A-Tall-Ship.local>
2018-10-25 21:51:35 +1100

    .

--- bmw-ibus/ibuslistener.rb
+++ bmw-ibus/ibuslistener.rb
@@ -33,7 +33,7 @@ class IBusListener
       @message.upcase!                         # Make the string uppercase
       @message = @message.scan(/.{1,2}/)       # Split the string into groups of two characters in an array.
       @message = IBusMessage.new(@message)     # Shove them into a new ibus message object
-      #@message.printRawMessage
+      @message.printRawMessage
       @messageContent = @message.printSourceDestination
       puts "Message Content Variable: #{@messageContent}"
       messageDetails = {"functionName" => "writeTitle", "functionDetails" => ["01",""], "content" => @messageContent}
